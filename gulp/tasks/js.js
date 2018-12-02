8a0f334c54dd9a01cb6f1bbe2a60d3c5cbfc61e2
kirlaban <kirlaban@mail.ru>
2018-12-02 15:43:04 +0400

    Summary
    Descr

---
+++ gulp/tasks/js.js
@@ -0,0 +1,48 @@
+module.exports = function () {
+	p.gulp.task("js", function () {
+		return (
+			p.gulp
+			.src(p.paths.src.js)
+			.pipe(p.gp.newer(p.paths.build.js))
+			.pipe(
+				p.gp.plumber({
+					errorHandler: p.gp.notify.onError(function (error) {
+						return {
+							title: "JavaScript - ошибка при сборке .js",
+							message: error.message,
+						};
+					}),
+				})
+			) // Предотвращает остановку плагина при возникновении ошибки
+			.pipe(develop ? p.gp.sourcemaps.init() : p.gp.util.noop()) // Инициализируем gulp-sourcemaps
+			// .pipe(
+			// 	p.gp.eslint({
+			// 		fix: true,
+			// 	})
+			// )
+			// .pipe(p.gp.eslint.format())
+			.pipe(develop ?
+				p.gp.util.noop() :
+				p.gp.babel({
+					presets: ["env"],
+				})
+			) // Babel
+			.pipe(p.gp.concat("app.min.js")) // Объединяем все JS файлы в один
+			// .pipe(develop ? p.gp.util.noop() : p.gp.uglify()) // Минифицируем наш js файл
+			.pipe(develop ? p.gp.sourcemaps.write() : p.gp.util.noop()) // Добавляем sourcemaps в файл .js
+			.pipe(p.gulp.dest(p.paths.build.js))
+			.pipe(p.webpack({
+				mode: 'production',
+				output: {
+					filename: 'app.min.js',
+				},
+			}))
+			.pipe(p.gulp.dest(p.paths.build.js))
+			.pipe(
+				p.browserSync.reload({
+					stream: true,
+				})
+			)
+		);
+	});
+};
